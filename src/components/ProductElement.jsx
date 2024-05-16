ee9f6f4f3614597a71a6728e30fa491bfe3fdc91
XurRound <xurround@gmail.com>
2024-05-16 14:00:12 +0300

    fixes

--- src/components/ProductElement.jsx
+++ src/components/ProductElement.jsx
@@ -57,8 +57,8 @@ const ProductElement = ({
                         {title}
                     </span>
                 </Link>
-                <p>{categories.find(i => i.category_id == category).name}</p>
-                <p>{manufacturers.find(i => i.manufacturer_id == manufacturerId).name}</p>
+                <p>{}</p>
+                <p>{}</p>
             </td>
             <td>
                 <span className="font-semibold text-xl tracking-tight mb-5 text-accent-content">

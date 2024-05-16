768f78e1f55b84f784298c16b9bf26d9d73017ab
XurRound <xurround@gmail.com>
2024-05-16 11:55:31 +0300

    fixes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,10 +5,7 @@ const Hero = () => {
     <div className="hero bg-base-200 bg-blend-overlay">
     <div className="hero-content text-center">
       <div className="max-w-xl">
-        <h1 className="text-5xl font-bold max-md:text-4xl text-accent-content">Best dropshipper ever!</h1>
-        <p className="py-6 text-2xl max-md:text-lg text-accent-content">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis ab delectus, nam aliquam ipsam quisquam error nostrum, tempora hic commodi quas nihil repellat cum architecto quod provident iure asperiores! Atque!
-        </p>
+        <h1 className="text-5xl font-bold max-md:text-4xl text-accent-content">Партнеры, добро пожаловать!</h1>
       </div>
     </div>
   </div>
